e9c987d144578f6f0db74e78ff264043d5bea367
Michał Wierzgoń <michal.wierzgon@gmail.com>
2024-04-25 21:16:47 +0200

    Update Techstack.jsx

--- src/components/sections/Techstack.jsx
+++ src/components/sections/Techstack.jsx
@@ -5,23 +5,22 @@ export default function Techstack() {
     <section className="tech-stack">
       <div className="tech-stack__icons-wrapper">
         <ul className="tech-stack__list_marquee-first">
-{techStack.map(item => {
-  return(
-    <li key={item.id}>
-    <i className={item.iconClass}></i>{item.techName}
-  </li>
-  )
-})}
+          {techStack.map(item => {
+            return(
+              <li key={item.id}>
+              <i className={item.iconClass}></i>{item.techName}
+            </li>
+            )
+          })}
         </ul>
         <ul className="tech-stack__list_marquee-second">
-        {techStack.map(item => {
-  return(
-    <li key={item.id}>
-    <i className={item.iconClass}></i>{item.techName}
-  </li>
-  )
-})}
-
+          {techStack.map(item => {
+            return(
+              <li key={item.id}>
+              <i className={item.iconClass}></i>{item.techName}
+            </li>
+            )
+          })}
         </ul>
       </div>
     </section>
